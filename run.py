17d52142847e5627d59fd2c90fffe3e1240ea912
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2020-12-20 13:40:38 +0100

    animation wip

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from gun import Gun
 from target import Target
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 # Setting the axes properties
 fig = plt.figure()
@@ -24,31 +25,46 @@ time = 0
 simulation_loop_time = 0.01
 
 target = Target(25000, 0, 30000, 50, 5, 20)
-gun = Gun(950, 80, 50, 10, 0)
+gun = Gun(950, 77, 50, 10, 0)
 
-while True:
-    shell = gun.get_shell_vector(time)
-    status = target.check_hit(shell)
-    ax.plot(shell[0], shell[2], shell[1], "bo")
+"""
+zapis lotu pocisku musi być w formie tablicy 3d
+gdzie każda kalumna jest kolejną iterqacją
+x 1 2 3
+y 1 2 3
+z 1 2 3
+potrzeba jest funkcja która w interwale bedzie odcyztywać itetator ww. wektora
+"""
+
+trajectory = np.empty([3, 0])
+
+def shoot(time, trajectory):
     ax.plot(target.x, target.z, target.y, "X")
-    if status == 1:
-        print("Target destroyed!!!!")
-        print(time)
-        print(shell)
-        print(target.coordinates)
-        break
-    elif status == 2:
-        print(time)
-        print("Target missed!!!!")
-        break
-    else:
-        # sleep(simulation_loop_time)
-        time += simulation_loop_time
-        continue
+    while True:
+        shell = gun.get_shell_vector(time)
+        status = target.check_hit(shell)
+        ax.plot(shell[0], shell[2], shell[1], ",")
+        vectors = np.array([[shell[0]], [shell[2]], [shell[1]]])
+        trajectory = np.append(trajectory, vectors, 1)
+        if status == 1:
+            print("Target destroyed!!!!")
+            print(time)
+            print(shell)
+            print(target.coordinates)
+            break
+        elif status == 2:
+            print(time)
+            print("Target missed!!!!")
+            break
+        else:
+            # sleep(simulation_loop_time)
+            time += simulation_loop_time
+            continue
+    return trajectory
 
-# for angle in range(0, 360):
-#     ax.view_init(30, angle)
-#     plt.draw()
-#     plt.pause(.001)
+
+trajectory = shoot(time, trajectory)
+print(trajectory.size)
 print(gun.get_shell_vector(time))
+
 plt.show()
