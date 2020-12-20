ab17035e444b116609ab51636fb26d5e2c4d38ce
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2020-12-20 22:59:09 +0100

    animation added

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ from time import sleep
 from gun import Gun
 from target import Target
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 import numpy as np
 
 status = 0
-simulation_loop_time = 0.01
+simulation_loop_time = 0.05
 trajectory = np.empty([3, 0])
 target = Target(25000, 0, 30000, 50, 5, 20)
-gun = Gun(950, 77, 50, 10, 0)
+gun = Gun(700, 77, 30, 10, 0)
 
 """
 zapis lotu pocisku musi być w formie tablicy 3d
@@ -23,12 +23,9 @@ potrzeba jest funkcja która w interwale bedzie odcyztywać itetator ww. wektora
 
 
 def shoot(time, vector):
-    ax.plot(0, 0, 0, "x", zdir="y")  # debug GUN
-    ax.plot(target.x, target.y, target.z, "X", zdir="y")   # debug TARGET
     while True:
         shell = gun.get_shell_vector(time)
         status = target.check_hit(shell)
-        ax.plot(shell[0], shell[1], shell[2], ",", zdir="y")   # debug SHELL TRAJECTORY SIMPLE
         vectors = np.array([[shell[0]], [shell[1]], [shell[2]]])
         vector = np.append(vector, vectors, 1)
         if status == 1:
@@ -38,32 +35,37 @@ def shoot(time, vector):
             print("Target missed!!!!")
             break
         else:
-            # sleep(simulation_loop_time)
             time += simulation_loop_time
             continue
     return vector
 
 
+def animate_trajectory(num, data_set, path):
+    path.set_data(data_set[:2, :num])
+    path.set_3d_properties(data_set[2, :num], zdir="y")
+    # print(path)
+    return path
+
+
+data = shoot(time=0, vector=trajectory)   # [0] = x, [1] = y, [2] = z
+
 fig = plt.figure()
-ax = p3.Axes3D(fig)
+ax = Axes3D(fig)
 
 ax.set_xlim3d([-500, 35000])
 ax.set_xlabel('X')
 
 ax.set_ylim3d([-500, 35000])
-ax.set_ylabel('Y')
+ax.set_ylabel('Z')
 
 ax.set_zlim3d([0, 2500])
-ax.set_zlabel('Z')
+ax.set_zlabel('Y')
 
 ax.set_title('3D Test')
 
-data = shoot(time=0, vector=trajectory)   # [0] = x, [1] = y, [2] = z
-line = [ax.plot(dat[0], dat[1], dat[2], zdir="y") for dat in data.T]   # shell trajectory vectors
-# lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
-# print(lines)
-# print(animate_trajectory(1, data, lines))
-# frames = trajectory.size
-# ani = animation.FuncAnimation(fig, animate_trajectory, frames, fargs=(data, line))
-#
+line = plt.plot(data[0], data[1], data[2], zdir="y", lw=2, c="g")[0]   # shell trajectory vectors
+frames = len(data.T)
+
+shell_animation = animation.FuncAnimation(fig, animate_trajectory, frames=frames, fargs=(data, line), interval=15, blit=False)
+shell_animation.save("shot_test.mp4")
 plt.show()
