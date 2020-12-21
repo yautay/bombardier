3b62d696230f206358b04bc52e8b4c2af4a20b78
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2020-12-21 08:09:49 +0100

    refactor

--- gun.py
+++ gun.py
@@ -1,39 +1,64 @@
+from target import Target
 import math
 import numpy as np
 
 
 class Gun:
     def __init__(self, vm, alpha, gamma, l, yb):
-        self.vm = vm   # muzzle velocity m/s
-        self.alpha = alpha   # vertical inclination
-        self.gamma = gamma   # horizontal azimuth
-        self.l = l   # barrel length
-        self.yb = yb   # gun elevation
-        self.dir_cos = self.__get_angle_cos()
-        self.barrel_coordinates = self.__get_barrel_coordinates()
+        self.__vm = vm   # muzzle velocity m/s
+        self.__alpha = alpha   # vertical inclination
+        self.__gamma = gamma   # horizontal azimuth
+        self.__l = l   # barrel length
+        self.__yb = yb   # gun elevation
+        self.__dir_cos = self.__get_angle_cos()
+        self.__barrel_coordinates = self.__get_barrel_coordinates()
+
 
     def __get_angle_cos(self):
-        b = self.l * math.cos((90 - self.alpha) * math.pi / 180)   # xy plain
-        lx = b * math.cos(self.gamma * math.pi / 180)   # barrel x part
-        ly = self.l * math.cos(self.alpha * math.pi / 180)   # barrel y part
-        lz = b * math.sin(self.gamma * math.pi / 180)   # barrel z part
-        cosx = lx / self.l
-        cosy = ly / self.l
-        cosz = lz / self.l
+        b = self.__l * math.cos((90 - self.__alpha) * math.pi / 180)   # xy plain
+        lx = b * math.cos(self.__gamma * math.pi / 180)   # barrel x part
+        ly = self.__l * math.cos(self.__alpha * math.pi / 180)   # barrel y part
+        lz = b * math.sin(self.__gamma * math.pi / 180)   # barrel z part
+        cosx = lx / self.__l
+        cosy = ly / self.__l
+        cosz = lz / self.__l
         directional_cos_array = np.array([cosx, cosy, cosz])
         return directional_cos_array
 
     def __get_barrel_coordinates(self):
-        xe = self.l * math.cos((90 - self.alpha) * math.pi / 180) * math.cos(self.gamma * math.pi / 180)   # barrel's end coordinate
-        ze = self.l * math.cos((90 - self.alpha) * math.pi / 180) * math.sin(self.gamma * math.pi / 180)   # barrel's end coordinate
+        xe = self.__l * math.cos((90 - self.__alpha) * math.pi / 180) * math.cos(self.__gamma * math.pi / 180)   # barrel's end coordinate
+        ze = self.__l * math.cos((90 - self.__alpha) * math.pi / 180) * math.sin(self.__gamma * math.pi / 180)   # barrel's end coordinate
         barrel_coordinates_array = np.array([xe, ze])
         return barrel_coordinates_array
 
-    def get_shell_vector(self, time):
-        si = self.vm * self.dir_cos[0] * time + self.barrel_coordinates[0]
-        sj = (self.yb + self.l * math.cos(self.alpha * math.pi / 180))\
-            + (self.vm * self.dir_cos[1] * time)\
+    def __get_shell_vector(self, time):
+        si = self.__vm * self.__dir_cos[0] * time + self.__barrel_coordinates[0]
+        sj = (self.__yb + self.__l * math.cos(self.__alpha * math.pi / 180))\
+            + (self.__vm * self.__dir_cos[1] * time)\
             - (0.5 * 9.81 * math.pow(time, 2))
-        sk = self.vm * self.dir_cos[2] * time + self.barrel_coordinates[1]
+        sk = self.__vm * self.__dir_cos[2] * time + self.__barrel_coordinates[1]
         vector = np.array([si, sj, sk])
         return vector
+
+    def shoot(self, target, loop_interval):
+        time = float(0)
+        vector = np.empty([3, 0])
+        if isinstance(target, Target):
+            while True:
+                shell = self.__get_shell_vector(time)
+                status = target.check_hit(shell)
+                vectors = np.array([[shell[0]], [shell[1]], [shell[2]]])
+                vector = np.append(vector, vectors, 1)
+                if status == 1:
+                    print("Target destroyed!!!!")
+                    break
+                elif status == 2:
+                    print("Target missed!!!!")
+                    break
+                else:
+                    time += loop_interval
+                    continue
+            return vector
+        else:
+            print("Target is not instance of Target class!!!")
+            raise Exception
--- run.py
+++ run.py
@@ -1,71 +1,37 @@
-from time import sleep
 from gun import Gun
 from target import Target
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
-import numpy as np
 
-status = 0
-simulation_loop_time = 0.05
-trajectory = np.empty([3, 0])
 target = Target(25000, 0, 30000, 50, 5, 20)
 gun = Gun(700, 77, 30, 10, 0)
 
-"""
-zapis lotu pocisku musi być w formie tablicy 3d
-gdzie każda kalumna jest kolejną iterqacją
-x 1 2 3
-y 1 2 3
-z 1 2 3
-potrzeba jest funkcja która w interwale bedzie odcyztywać itetator ww. wektora
-"""
-
-
-def shoot(time, vector):
-    while True:
-        shell = gun.get_shell_vector(time)
-        status = target.check_hit(shell)
-        vectors = np.array([[shell[0]], [shell[1]], [shell[2]]])
-        vector = np.append(vector, vectors, 1)
-        if status == 1:
-            print("Target destroyed!!!!")
-            break
-        elif status == 2:
-            print("Target missed!!!!")
-            break
-        else:
-            time += simulation_loop_time
-            continue
-    return vector
+data = gun.shoot(target=target, loop_interval=0.1)   # [0] = x, [1] = y, [2] = z
 
 
 def animate_trajectory(num, data_set, path):
     path.set_data(data_set[:2, :num])
     path.set_3d_properties(data_set[2, :num], zdir="y")
-    # print(path)
     return path
 
-
-data = shoot(time=0, vector=trajectory)   # [0] = x, [1] = y, [2] = z
 
 fig = plt.figure()
 ax = Axes3D(fig)
 
 ax.set_xlim3d([-500, 35000])
 ax.set_xlabel('X')
-
 ax.set_ylim3d([-500, 35000])
 ax.set_ylabel('Z')
-
 ax.set_zlim3d([0, 2500])
 ax.set_zlabel('Y')
-
-ax.set_title('3D Test')
 
 line = plt.plot(data[0], data[1], data[2], zdir="y", lw=2, c="g")[0]   # shell trajectory vectors
 frames = len(data.T)
 
+
 shell_animation = animation.FuncAnimation(fig, animate_trajectory, frames=frames, fargs=(data, line), interval=15, blit=False)
 shell_animation.save("shot_test.mp4")
+
+
 plt.show()
